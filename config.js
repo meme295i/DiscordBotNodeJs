f01f11e101e28c909faf42795b3fdb0203225659
meme295i <44748574+meme295i@users.noreply.github.com>
2018-11-06 08:31:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 exports.config = {
     // bot owner discord id
-    OWNER_ID: "101041126537973760",
+    OWNER_ID: "",
 
     // bot prefix
     PREFIX: ".",
 
     // channel id's to ignore
-    IGNORE_CHANNELS: ["397961894654246913"],
+    IGNORE_CHANNELS: [""],
 
     // where to send welcome message (guildID : channelID)
     WELCOME_MESSAGE_CHANNEL: {
-        "408735794094669824": "408737565298917397",
+        "": "",
         //"144518146232025088": "193151740743385089"
     }
 };
